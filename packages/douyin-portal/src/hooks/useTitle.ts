94dbed5362a02729a8a466cd31c738d2a7152cce
mafqla <1692285748@qq.com>
2024-01-16 10:42:32 +0800

    feat(*): all

---
+++ packages/douyin-portal/src/hooks/useTitle.ts
@@ -0,0 +1,19 @@
+import { onUnmounted, ref, watchEffect } from 'vue'
+
+export function useTitle(title: string, restoreOnUnmount = true) {
+  const cachedTitle = document.title
+  const titleRef = ref(title)
+
+  watchEffect(() => { 
+    document.title = titleRef.value
+  })
+  if (restoreOnUnmount) {
+    onUnmounted(() => { 
+      document.title = cachedTitle
+    })
+  }
+  const setTitle = (newTitle: string) => {
+    titleRef.value = newTitle
+  }
+  return setTitle
+}
